067c60f68339e730341de1da8afbec2327573b36
mohamed <khaldimohamed82@gmail.com>
2025-02-17 18:44:45 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- code/test.c
+++ code/test.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main (void)
+int	main(void)
 {
-int a[10];
-int count;
-for (count = 0; count < 10; count++)
-{
-a[count] = count * 10 + count;
-}
-printf ("The first and second elements of a are %d and %d\n",
-a[0], a[1]);
-printf ("Or, as pointers, %d and %d\n", *a, *(a+1));
-return 0;
+	int	a[10];
+	int	count;
+
+	for (count = 0; count < 10; count++)
+	{
+		a[count] = count * 10 + count;
+	}
+	printf("The first and second elements of a are %d and %d\n",
+			a[0],
+			a[1]);
+	printf("Or, as pointers, %d and %d\n", *a, *(a + 1));
+	return (0);
 }
--- code/test_1.c
+++ code/test_1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main (void)
+int	main(void)
 {
-int intval = 255958283;
-void *vptr = &intval;
-printf ("The value at vptr as an int is %d\n", *((int *) vptr));
-printf ("The value at vptr as a char is %d\n", *((char *) vptr));
-return 0;
+	int		intval;
+	void	*vptr;
+
+	intval = 255958283;
+	vptr = &intval;
+	printf("The value at vptr as an int is %d\n", *((int *)vptr));
+	printf("The value at vptr as a char is %d\n", *((char *)vptr));
+	return (0);
 }
