19ab2bc31372a99e3501b30a61bd03bd34ee0484
mohamed <khaldimohamed82@gmail.com>
2025-03-16 08:26:40 +0000

    Expanding my Skillset, Embracing Continuous Learning

--- code/rostring.c
+++ code/rostring.c
@@ -30,8 +30,7 @@ int	main(int ac, char **av)
 					i++;
 				while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
 					write(1, &av[1][i++], 1);
-				if (av[1][i])
-					write(1, " ", 1);
+				write(1, " ", 1);
 			}
 		}
 		else
