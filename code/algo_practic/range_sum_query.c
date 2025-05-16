8f99ba195b8c1578852bece1567e45b039d5c925
mohamed <khaldimohamed82@gmail.com>
2025-05-16 19:08:01 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ code/algo_practic/range_sum_query.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+long long	arr[100000];
+int	main(void)
+{
+	long long	i;
+	long long	sum;
+
+	int n, q, l, r;
+	scanf("%d", "%d", &n, &q);
+	i = 0;
+	while (++i <= n)
+	{
+		scanf("%lld", &arr[i]);
+	}
+	i = 0;
+	sum = 0;
+	scanf("%d", "%d", &l, &r);
+	while (l <= r)
+	{
+		sum += arr[i];
+		i++;
+		l++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
