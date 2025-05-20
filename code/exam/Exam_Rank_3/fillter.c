07c004ef94ff368509ef5ea43915e72156f6ca76
mohamed <khaldimohamed82@gmail.com>
2025-05-20 20:33:22 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- code/exam/Exam_Rank_3/fillter.c
+++ code/exam/Exam_Rank_3/fillter.c
@@ -1,20 +1,35 @@
-#include <cmath>
+// #include <cmath>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int	ft_error(char *s)
+{
+	perror(s);
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
 	char	*s;
-	int		i;
+	char	*str;
 
+	str = argv[1];
+	s = malloc(1048);
 	if (argc < 2)
+		ft_error("arg error");
+	while (1)
 	{
-		perror("missing argemment");
-		return (1);
-	}
-	s = *argv;
-	i = 0;
-	while (s[i])
-	{
+		if (read(0, s, 1048) == -1)
+			ft_error("read error");
+		if (strcmp(str, s) == 0)
+		{
+			write(1, "indenticle\n", 11);
+			return (0);
+		}
+		else
+			printf("try agin ):");
 	}
 	return (0);
 }
