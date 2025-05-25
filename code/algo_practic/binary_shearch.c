889611eeaf0df161fc8d91b5f57ccce7de97c165
mohamed <khaldimohamed82@gmail.com>
2025-05-25 06:32:39 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- code/algo_practic/binary_shearch.c
+++ code/algo_practic/binary_shearch.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+// #include <stdlib.h>
+// #include <string.h>
 
 int	search(int *nums, int numsSize, int target)
 {
@@ -12,26 +13,26 @@ int	search(int *nums, int numsSize, int target)
 	min = 0;
 	while (min <= max)
 	{
-		mid = max - min / 2;
+		mid = min + (max - min) / 2;
 		if (nums[mid] == target)
 			return (mid);
-		else if (nums[mid] < target)
+		if (nums[mid] < target)
 			min = mid + 1;
 		else
 			max = mid - 1;
 	}
-	return (0);
+	return (-1);
 }
 
 int	main(void)
 {
 	int	tar;
-	int	arr[6];
 	int	r;
+	int	arr[6];
 
-	arr[6] = {-1, 0, 3, 5, 9, 12};
-	tar = 12;
-	r = search(arr, 5, tar);
+	arr = {-1, 0, 3, 5, 9, 12};
+	tar = 9;
+	r = search(arr, 6, tar);
 	printf("%d\n", r);
 	return (0);
 }
